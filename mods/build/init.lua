52a250da68577074f9ac20ccf684e5282768702d
PilzAdam <pilzadam@minetest.net>
2013-03-23 17:54:38 +0100

    Add build mod with renewable lava

---
+++ mods/build/init.lua
@@ -0,0 +1,66 @@
+-- Minetest 0.4 mod: build
+-- See README.txt for licensing and other information.
+
+minetest.register_node(":default:lava_flowing", {
+	description = "Flowing Lava",
+	inventory_image = minetest.inventorycube("default_lava.png"),
+	drawtype = "flowingliquid",
+	tiles = {"default_lava.png"},
+	special_tiles = {
+		{
+			image="default_lava_flowing_animated.png",
+			backface_culling=false,
+			animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=3.3}
+		},
+		{
+			image="default_lava_flowing_animated.png",
+			backface_culling=true,
+			animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=3.3}
+		},
+	},
+	paramtype = "light",
+	light_source = LIGHT_MAX - 1,
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	drop = "",
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "default:lava_flowing",
+	liquid_alternative_source = "default:lava_source",
+	liquid_viscosity = LAVA_VISC,
+	damage_per_second = 4*2,
+	post_effect_color = {a=192, r=255, g=64, b=0},
+	groups = {lava=3, liquid=2, hot=3, igniter=1, not_in_creative_inventory=1},
+})
+
+minetest.register_node(":default:lava_source", {
+	description = "Lava Source",
+	inventory_image = minetest.inventorycube("default_lava.png"),
+	drawtype = "liquid",
+	tiles = {
+		{name="default_lava_source_animated.png", animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=3.0}}
+	},
+	special_tiles = {
+		-- New-style lava source material (mostly unused)
+		{
+			name="default_lava_source_animated.png",
+			animation={type="vertical_frames", aspect_w=16, aspect_h=16, length=3.0},
+			backface_culling = false,
+		}
+	},
+	paramtype = "light",
+	light_source = LIGHT_MAX - 1,
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	drop = "",
+	liquidtype = "source",
+	liquid_alternative_flowing = "default:lava_flowing",
+	liquid_alternative_source = "default:lava_source",
+	liquid_viscosity = LAVA_VISC,
+	damage_per_second = 4*2,
+	post_effect_color = {a=192, r=255, g=64, b=0},
+	groups = {lava=3, liquid=2, hot=3, igniter=1},
+})
